fdff5e2cf5893b394f4b812ada291b87e032d04a
Mohamed Arafa <m3arafa906@gmail.com>
2018-03-29 16:51:01 +0200

    end of video 243 - categories created and updated and deleting work will

--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -1,7 +1,39 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: m3arafa
- * Date: 24/03/18
- * Time: 04:48 م
- */
+@extends('layouts.admin')
+
+
+@section('content')
+
+
+
+    <h1>Categories:</h1>
+
+    <div class="row">
+
+        <div class="col-md-6">
+            {!! Form::model($category, ['method'=>'PATCH','action'=>['AdminCategoriesController@update', $category->id]]) !!}
+
+            <div class="form-group">
+                {!! Form::label('name','Name:') !!}
+                {!! Form::text('name', null , ['class'=>'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::submit('Update Category',['class'=>'btn btn-primary col-sm-6']) !!}
+            </div>
+
+            {!! Form::close() !!}
+
+            {{-- form Delete--}}
+
+            {!! Form::open(['method'=>'DELETE','action'=>['AdminCategoriesController@destroy', $category->id]]) !!}
+
+            <div class="form-group">
+                {!! Form::submit('Delete Category',['class'=>'btn btn-danger col-sm-6']) !!}
+            </div>
+
+            {!! Form::close() !!}
+
+        </div>
+    </div>
+
+@stop
--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -1,7 +1,63 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: m3arafa
- * Date: 24/03/18
- * Time: 04:47 م
- */
+@extends('layouts.admin')
+
+
+@section('content')
+
+
+
+    <h1>Categories:</h1>
+
+    <div class="row">
+        <div class="col-md-5">
+
+
+            {!! Form::open(['method'=>'POST','action'=>'AdminCategoriesController@store']) !!}
+
+            <div class="form-group">
+                {!! Form::label('name','Name:') !!}
+                {!! Form::text('name', null , ['class'=>'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::submit('create Category',['class'=>'btn btn-primary']) !!}
+            </div>
+
+            {!! Form::close() !!}
+
+
+        </div>
+        <div class="col-md-7">
+
+            @if($categories)
+
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Created date</th>
+                        <th>Updated date</th>
+                    </tr>
+                    </thead>
+
+                    <tbody>
+                    @foreach($categories as $category)
+                        <tr>
+                            <td>{{$category->id}}</td>
+                            <td><a href="{{route('admin.categories.edit', $category->id)}}">{{$category->name}}</a></td>
+                            <td>{{$category->created_at ? $category->created_at->diffForHumans() : "No Date"}}</td>
+                            <td>{{$category->updated_at ? $category->updated_at->diffForHumans() : "No Date"}}</td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+
+            @endif
+
+        </div>
+
+    </div>
+
+
+
+@stop
